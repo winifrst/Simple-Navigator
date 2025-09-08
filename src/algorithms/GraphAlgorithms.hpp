0b24521f7c3a4ff31f45a7932be4e085715643a1
satvajos <satvajos@student.21-school.ru>
2025-09-08 22:16:03 +0300

    добавила алгоритм Флойда-Уоршелла

--- src/algorithms/GraphAlgorithms.hpp
+++ src/algorithms/GraphAlgorithms.hpp
@@ -6,6 +6,8 @@ class GraphAlgorithms {
 public:
   static long long GetShortestPathBetweenVertices(Graph &graph, int vertex1,
                                                   int vertex2);
+  static std::vector<std::vector<int>>
+  GetShortestPathsBetweenAllVertices(Graph &graph);
 
 private:
   GraphAlgorithms() = default;
