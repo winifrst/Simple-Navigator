5460bedce1ac51977e2ccbc58f2d62a26c42a7c6
satvajos <satvajos@student.21-school.ru>
2025-09-12 18:14:44 +0300

    убрала функцию, которую вынесла в отдельный файл

--- src/tests/TestsFloydWarshall.cpp
+++ src/tests/TestsFloydWarshall.cpp
@@ -1,25 +1,8 @@
 #include "../algorithms/GraphAlgorithms.hpp"
+#include "TestUtils.hpp"
 #include <gtest/gtest.h>
 
 const int INF_INT = std::numeric_limits<int>::max();
-bool MatricesEqual(const std::vector<std::vector<int>> &a,
-                   const std::vector<std::vector<int>> &b) {
-  if (a.size() != b.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i].size() != b[i].size()) {
-      return false;
-    }
-
-    for (size_t j = 0; j < a[i].size(); ++j) {
-      if (a[i][j] != b[i][j]) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
 
 TEST(FloydWarshallAlgorithmTest, SingleVertex) {
   Graph graph;
